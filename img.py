7f0114bd8bedb0252ef969625701b76c2d7d9e0d
Soham Chakrabarti <soham17041998@gmail.com>
2017-12-25 16:31:50 +0530

    Update img.py

--- img.py
+++ img.py
@@ -3,7 +3,7 @@ import matplotlib.image as mpimg
 import numpy as np
 
 
-maze = mpimg.imread('../py/dog.png')
+maze = mpimg.imread('../dog.png')
 
 
 print("------------------------------")
@@ -11,13 +11,13 @@ x=0
 y=10
 rgb=0.0000
 c=0
-
+#print the the pixels of the image in ndarray
 print(maze)
-
+#iterate to plot image
 for row in maze:
    for row1 in row:
     for row2 in row1:
-     rgb=rgb+row2 
+     rgb=rgb+row2 #calculate the average RGB
     
     rgb=rgb/3
     if (rgb!=0.0000000):
@@ -26,14 +26,14 @@ for row in maze:
     else:
           plt.plot(x,y,'bo')
           c+=1     
-    print (rgb,c)
+    print (rgb,c)#print RGB and counter to keep track
     x+=.5
     rgb=0.000
    
    y-=1; x=0; 
 
 
-plt.axis([-5,200,-200,15])
+plt.axis([-5,200,-200,15]) #ploting in graph
 plt.show()
    
    
